85e497f4dd64deb05351e33b1e8a2f5bd94d2705
Olha-buz <134137848+Olha-buz@users.noreply.github.com>
2024-01-04 22:26:44 +0200

    fix/modalcss

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,14 +1,16 @@
-import css from './Modal.modaule.css';
+import css from './Modal.module.css';
 const { Component } = require("react");
 
 
 class Modal extends Component {
     componentDidMount() {
         window.addEventListener('keydown', this.handleKeyDown);
+        window.addEventListener('click', this.handleBackdropClick);
     }
 
     componentWillUnmount() {
         window.removeEventListener('keydown', this.handleKeyDown);
+        window.removeEventListener('click', this.handleBackdropClick);
     }
 
     handleKeyDown = evt => {
@@ -28,12 +30,12 @@ class Modal extends Component {
         return (
             <div className={css.overlay} onClick={this.handleBackdropClick}>
                 <div className={css.modal}>
-                    <img className={css.imgmodal} src={url} alt={alt} onClick={this.handleBackdropClick}/>
+                    <img className={css.imgmodal} src={url} alt={alt} onClick={this.handleBackdropClick} />
                     <button className={css.btnClose} onClick={this.handleBackdropClick}>
                         X
                     </button>
                 </div>
-            </div>            
+            </div>
         );
     }
 }
